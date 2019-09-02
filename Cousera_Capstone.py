0bb9674850627b7480d45d4c641a2af3d0d52bf2
cenyupian <48969074+cenyupian@users.noreply.github.com>
2019-09-01 22:29:59 -0400

    Capstone Project Notebook

---
+++ Cousera_Capstone.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#this notebook will be mainly used for the capstone project.
+
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+import numpy as np
+
+
+# In[7]:
+
+
+print("Hello Capstone Project Course!")
+
